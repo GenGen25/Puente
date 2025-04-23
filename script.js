4f48c27292d4b7f05a31412de72b5cab39da827f
GenBen10 <genesis.magally.vasquez17@gmail.com>
2025-04-23 14:06:02 -0700

    Fix all questions
    Made it more like a jeopardy game
    - need to add audio
    - fix grammar

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const modal = document.getElementById('modal');
 const overlay = document.getElementById('overlay');
 const modalText = document.getElementById('modalText');
 const openBtns = document.querySelectorAll('.openModal');
+const closeBtn = document.getElementById('closeBtn');
+
+let currentButton = null;
 
 openBtns.forEach(btn => {
   btn.addEventListener('click', () => {
@@ -9,10 +12,18 @@ openBtns.forEach(btn => {
     modalText.textContent = message;
     modal.classList.add('show');
     overlay.classList.add('show');
+
+    currentButton = btn;
   });
 });
 
-function closeModal() {
+
+closeBtn.addEventListener('click', () => {
   modal.classList.remove('show');
   overlay.classList.remove('show');
-}
+    if (currentButton){
+      currentButton.style.display = 'none';
+      currentButton = null;
+    }
+});
+
